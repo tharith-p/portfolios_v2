a10ec7c62a80d789fc6bcf810930329c95769e40
ing <phorntharith090@gmail.com>
2024-02-11 01:51:53 +0700

    asdf

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,12 +3,10 @@ module.exports = {
     apps: [
         {
             name: 'tharith_portfolio',
-            port: '3005',
+            port: '3031',
             exec_mode: 'cluster',
-            instances: '1',
-            script: 'bun',
-            watch: true,
-            args: 'run dev',
+            instances: '2',
+            script: './.output/server/index.mjs',
             env: {}
         }
     ]
